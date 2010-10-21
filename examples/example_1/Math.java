cab2e3299c3fbe483004c223fe37fcb29826a712
Douglas Squirrel <douglas.squirrel@youdevise.com>
2010-10-21 18:31:13 +0100

    Add files on which to build example

---
+++ examples/example_1/Math.java
@@ -0,0 +1,9 @@
+public class Math {
+        static public int add(int a, int b) {  
+                return a + b;
+        }
+
+        public static void main(String[] args) {
+                System.out.println("2+2=" + Math.add(2,2));
+        }
+} 
