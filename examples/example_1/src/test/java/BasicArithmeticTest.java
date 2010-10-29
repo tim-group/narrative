0d6f6653255548aa6b5ba427c2cf977e5f39149b
Andrew Parker <andrew.parker@youdevise.com>
2010-10-29 13:56:29 +0100

    fixing the example. Seems to have lost the changes from the zaphod42 fork

--- examples/example_1/src/test/java/BasicArithmeticTest.java
+++ examples/example_1/src/test/java/BasicArithmeticTest.java
@@ -16,7 +16,7 @@ public class BasicArithmeticTest {
         You need to define an Actor to get started. The Actor performs various actions on the domain
         objects - in this case we have just one object, the calculator.
     */
-    private Actor<Calculator> operator = new CalculatorActor();
+    private CalculatorActor operator = new CalculatorActor();
 
     /*
         The tests themselves, in given-when-then style. Narrative provides everything used here
@@ -49,10 +49,10 @@ public class BasicArithmeticTest {
         Here we define press() and the_displayed_value(). They just delegate to the corresponding
         actions on the calculator.
     */
-    private static Action<Calculator> press(final char keypress) {
-        return new Action<Calculator>() {
-            public void performFor(Calculator calculator, Stash stash) {
-                calculator.press(keypress);
+    private static Action<Calculator, CalculatorActor> press(final char keypress) {
+        return new Action<Calculator, CalculatorActor>() {
+            public void performFor(CalculatorActor actor) {
+                actor.tool().press(keypress);
             }
         };
     }
@@ -68,15 +68,19 @@ public class BasicArithmeticTest {
     /*
         The Actor that does all the work as commanded by the tests.
     */
-    private static class CalculatorActor implements Actor<Calculator> {
+    private static class CalculatorActor implements Actor<Calculator, CalculatorActor> {
         private Calculator calculator = new Calculator();
 
         public Calculator tool() {
             return calculator;
         }
 
-        public <DATA> DATA grabUsing(Extractor<DATA, Calculator> extractor) {
-            return extractor.grabFrom(calculator, new HashMapStash());
+        public void perform(Action<Calculator, CalculatorActor> action) {
+            action.performFor(this);
+        }
+
+        public <DATA> DATA grabUsing(Extractor<DATA, CalculatorActor> extractor) {
+            return extractor.grabFor(this);
         }
     }
 }
