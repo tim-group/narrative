97b760764326b08b19ff75866059ef07b221ae55
Samir Talwar <samir.talwar@youdevise.com>
2010-11-26 16:41:51 +0000

    Missed one.

--- src/main/java/com/youdevise/test/narrative/Action.java
+++ src/main/java/com/youdevise/test/narrative/Action.java
@@ -4,7 +4,7 @@ package com.youdevise.test.narrative;
  * An action that can be performed by an Actor that changes the state of the
  * system.
  *
- * @param <T>
+ * @param <TOOL>
  *            The type of the tool used by the Actor.
  */
 public interface Action<TOOL, ACTOR extends Actor<TOOL, ?>> {
